6fc8265639f7775e94c01157cce8cbaca7a2ade1
Peter Wise <peter@bloodaxe.com>
2018-08-22 14:21:37 +0100

    Basic agency list
    ref #8

---
+++ jobtrack2/views/agency.py
@@ -0,0 +1,32 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+
+from sqlalchemy.exc import DBAPIError
+
+from ..models import Agency
+
+@view_config(route_name='agency_list', renderer='../templates/agencylist.jinja2')
+def agencylist(request):
+    try:
+        query = request.dbsession.query(Agency)
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
+    return {'agencies': query.all(), 'project': 'JobTrack2'}
+
+
+#TODO: Probably should put this error message somewhere central
+db_err_msg = """\
+Pyramid is having a problem using your SQL database.  The problem
+might be caused by one of the following things:
+
+1.  You may need to run the "initialize_jobtrack2_db" script
+    to initialize your database tables.  Check your virtual
+    environment's "bin" directory for this script and try to run it.
+
+2.  Your database server may not be running.  Check that the
+    database server referred to by the "sqlalchemy.url" setting in
+    your "development.ini" file is running.
+
+After you fix the problem, please restart the Pyramid application to
+try it again.
+"""
